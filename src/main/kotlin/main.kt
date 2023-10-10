a98152df117968e93849ad8150e12d7e4f4023e9
naum mazor <maznam1@mail.ru>
2023-10-10 21:27:13 +0300

    Changed main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,26 +1,26 @@
 fun main() {
 
     val price = 7000
-    val discontOverage = 100
-    val discontLargePercent = 0.05
+    val discountOverage = 100
+    val discountLargePercent = 0.05
     val regCustomer = true
-    val priceWithDiscont: Int
+    val priceWithDiscount: Int
     val priceRegCustomer: Int
 
     println("покупка - " + price + " рублей")
 
     if (price <= 1000) {
-        priceWithDiscont = price
+        priceWithDiscount = price
     } else if (price > 1000 && price <= 10000) {
-        priceWithDiscont = price - discontOverage
-        println("после применения скидки в 100 рублей - " + priceWithDiscont + " рублей.")
+        priceWithDiscount = price - discountOverage
+        println("после применения скидки в 100 рублей - " + priceWithDiscount + " рублей.")
     } else {
-        priceWithDiscont = price - (price * discontLargePercent).toInt()
-        println("после применения 5% скидки - " + priceWithDiscont + " рублей.")
+        priceWithDiscount = price - (price * discountLargePercent).toInt()
+        println("после применения 5% скидки - " + priceWithDiscount + " рублей.")
     }
 
     if (regCustomer) {
-        priceRegCustomer = priceWithDiscont - (priceWithDiscont * 0.01).toInt()
+        priceRegCustomer = priceWithDiscount - (priceWithDiscount * 0.01).toInt()
         println("после применения 1% скидки — " + priceRegCustomer + " рублей.")
     }
 }
